0f83076505a7cc4d4a23be908765f9c0043e1e11
Ragib <atmragib06@student.sust.edu>
2020-06-24 19:08:45 +0600

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jadukor',
+      version='0.1',
+      description='The most powerfull module in the universe',
+      url='http://github.com/atmragib/jadukor',
+      author='A T M Ragib Raihan',
+      author_email='atmragibraihan@gmail.com',
+      license='MIT',
+      packages=['jadukor'],
+      zip_safe=False)
