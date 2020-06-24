c89fa70b8acdb27eb909195bc3269ab3dc806624
Ragib <atmragib06@student.sust.edu>
2020-06-25 01:58:02 +0600

    setup.py file structure reformated according to some popular conventions.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from distutils.core import setup
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+base_url = 'http://github.com/atmragib/jadukor'
+version = '0.0.3'
+
 setup(name='jadukor',
-      version='0.2',
+      version=version,
       description='The most powerfull module in the universe',
-      url='http://github.com/atmragib/jadukor',
-      download_url='https://github.com/atmragib/jadukor/archive/v0.2.tar.gz',
+      long_description=long_description,
+      url=base_url,
+      download_url='{}/archive/v{}.tar.gz'.format(base_url, version),
       author='A T M Ragib Raihan',
       author_email='atmragibraihan@gmail.com',
       license='MIT',
@@ -19,11 +26,10 @@ setup(name='jadukor',
       classifiers=[
           # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
           'Development Status :: 3 - Alpha',
-          # Define that your audience are developers
           'Intended Audience :: Developers',
           'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: MIT License',   # Again, pick a license
-          # Specify which pyhton versions that you want to support
+          'License :: OSI Approved :: MIT License',
+          "Operating System :: OS Independent",
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
@@ -31,4 +37,10 @@ setup(name='jadukor',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
       ],
-)
+      python_requires='>=3.1',
+    #   packages=find_packages(exclude=['tests', 'tests.*']),
+    #   package_data={'simple_history': [
+    #       'static/js/*.js', 'templates/simple_history/*.html']},
+    #   include_package_data=True,
+    # use_scm_version=True,
+      )
